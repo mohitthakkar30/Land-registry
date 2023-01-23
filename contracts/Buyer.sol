d9e0b387f69cbfa90a908d28311f4eb60a1007b7
Mohit Thakkar <45704711+mohitthakkar30@users.noreply.github.com>
2023-01-23 21:51:12 +0530

    First Commit

---
+++ contracts/Buyer.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+contract Buyer {
+    struct BuyerDetails {
+        address id;
+        string name;
+        uint256 age;
+        string aadharNumber;
+        string panNumber;
+        string city;
+        string state;
+      
+    }
+
+    function AddBuyerDetails(
+        address _id,
+        string memory _name,
+        uint256 _age,
+        string memory _city,
+        string memory _state,
+        string memory _adharNumber,
+        string memory _panNumber
+    ) internal pure returns (BuyerDetails memory) {
+        return
+            BuyerDetails(
+                _id,
+                _name,
+                _age,
+                _city,
+                _state,
+                _adharNumber,
+                _panNumber
+            );
+    }
+}
